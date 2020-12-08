1cede88331754aacf66c346b92728431091bd7f3
David Potter <destroyonload@gmail.com>
2020-12-08 15:47:19 +0800

    init commit

---
+++ FindWebsiteUrl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindWebsiteUrl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
